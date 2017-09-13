92459de0f1d5b56079d5374bf48064d93cef25b6
Todd Jones <anthony.jones@cgifederal.com>
2017-09-13 14:08:48 -0400

    First version

---
+++ dot.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // var totalMessages = 1400000;
+  // var accessibleMessages = 10000;
+  // var inaccessibleMessages = totalMessages - accessibleMessages;
+  // var accessibleDivs = '';
+  // var inaccessibleDivs = '';
+  // for(var i=0, j=1000; i<j; i++) {
+  //   accessibleDivs+='<i class="fa fa-circle darkblue" aria-hidden="true"></i>';
+  // }
+  // for(var i=0, j=140000; i<j; i++) {
+  //   inaccessibleDivs+='<i class="fa fa-circle lightblue" aria-hidden="true"></i>';
+  // }
+  // $('main').append($(accessibleDivs)).append($(inaccessibleDivs));
+});
